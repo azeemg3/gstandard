bd12bbcc1ac1843f0a7756997711854fae6c19a9
Muhammad Azeem Khalid <azeemkhalidg3@gmail.com>
2025-11-15 19:01:49 +0500

    updatehomeui

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
         <div class="row">
-            <div class="col-lg-3 col-6">
+            <div class="col-lg-3 col-6 mb-4">
                 <!-- small box -->
                 <div class="small-box bg-info">
                     <div class="inner">
@@ -21,7 +21,7 @@
                 </div>
             </div>
             <!-- ./col -->
-            <div class="col-lg-3 col-6">
+            <div class="col-lg-3 col-6 mb-4">
                 <!-- small box -->
                 <div class="small-box bg-success">
                     <div class="inner">
@@ -36,7 +36,7 @@
                 </div>
             </div>
             <!-- ./col -->
-            <div class="col-lg-3 col-6">
+            <div class="col-lg-3 col-6 mb-4">
                 <!-- small box -->
                 <div class="small-box bg-warning">
                     <div class="inner">
@@ -51,7 +51,7 @@
                 </div>
             </div>
             <!-- ./col -->
-            <div class="col-lg-3 col-6">
+            <div class="col-lg-3 col-6 mb-4">
                 <!-- small box -->
                 <div class="small-box bg-danger">
                     <div class="inner">
@@ -73,51 +73,53 @@
             <!-- Left col -->
             <section class="col-lg-12 connectedSortable">
                 <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">
-                                Recent Transactions
+                    <div class="card shadow-sm mb-4">
+                        <div class="card-header border-0 d-flex align-items-center justify-content-between">
+                            <h3 class="card-title mb-0">
+                                <i class="fas fa-exchange-alt mr-2"></i> Recent Transactions
                             </h3>
                         </div>
                         <div class="card-body">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th style="width: 10px">#</th>
-                                        <th>MTCN</th>
-                                        <th>Amount</th>
-                                        <th>Sender</th>
-                                        <th>Receiver</th>
-                                        <th>Status</th>
-                                        <th style="width: 40px">Branch</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($data as $item)
-                                    @php
-                                    $senderDetails = json_decode($item->sender_details);
-                                    $receiverDetails = json_decode($item->receiver_details);
-                                    @endphp
-                                    <tr>
-                                        <td>{{ $loop->iteration }}.</td>
-                                        <td>{{ $item->transaction_code }}</td>
-                                        <td>{{ $item->amount }}</td>
-                                        <td>{{ $senderDetails->sender_name  }} ({{ $senderDetails->sender_mobile }})</td>
-                                        <td>{{ $receiverDetails->receiver_name }} ({{ $receiverDetails->receiver_mobile }})</td>
-                                        <td>
-                                            @if ($item->status == 'delivered')
-                                            <span class="badge bg-success">{{ $item->status }}</span>
-                                            @elseif ($item->status == 'approved')
-                                            <span class="badge bg-success">{{ $item->status }}</span>
-                                            @else
-                                            <span class="badge bg-warning">{{ $item->status }}</span>
-                                            @endif
-                                        </td>
-                                        <td>{{ $item->receiver_branch->name }}</td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-striped table-hover table-sm mb-0">
+                                    <thead class="thead-light">
+                                        <tr>
+                                            <th style="width: 10px">#</th>
+                                            <th>MTCN</th>
+                                            <th>Amount</th>
+                                            <th>Sender</th>
+                                            <th>Receiver</th>
+                                            <th>Status</th>
+                                            <th style="width: 40px">Branch</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($data as $item)
+                                        @php
+                                            $senderDetails = json_decode($item->sender_details);
+                                            $receiverDetails = json_decode($item->receiver_details);
+                                        @endphp
+                                        <tr>
+                                            <td class="text-muted">{{ $loop->iteration }}.</td>
+                                            <td class="text-monospace">{{ $item->transaction_code }}</td>
+                                            <td class="text-nowrap">{{ $item->amount }}</td>
+                                            <td>{{ $senderDetails->sender_name  }} ({{ $senderDetails->sender_mobile }})</td>
+                                            <td>{{ $receiverDetails->receiver_name }} ({{ $receiverDetails->receiver_mobile }})</td>
+                                            <td>
+                                                @if ($item->status == 'delivered')
+                                                    <span class="badge bg-success">{{ $item->status }}</span>
+                                                @elseif ($item->status == 'approved')
+                                                    <span class="badge bg-success">{{ $item->status }}</span>
+                                                @else
+                                                    <span class="badge bg-warning">{{ $item->status }}</span>
+                                                @endif
+                                            </td>
+                                            <td>{{ $item->receiver_branch->name }}</td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
